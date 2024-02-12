705731ca3935ec85912a1a0fe76e71b7797225b7
Mohammad Ali Ashraf <ashraf182003@gmail.com>
2024-02-12 01:07:52 +0100

    Footer created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from "./pages/Dashboard"
 import Signin from "./pages/Signin"
 import Projects from "./pages/Projects"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
       <Route path="/projects" element={<Projects/>}/>
       <Route path="/*" element={<ErrorPage />} />
     </Routes>
+    <Footer/>
     </BrowserRouter>
   )
 }
